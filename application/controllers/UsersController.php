a89ad5f3a4e146eca6fde3af3caaddf3a300ba9c
vtzika <varvara.tzika@live.com>
2014-11-06 12:04:19 +0100

    register action added

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -31,7 +31,29 @@ class UsersController extends Zend_Controller_Action
     }
 
 
-    
+     public function registerAction()
+        {
+            $users = new Application_Model_DbTable_Users();
+            #$this->view->user = $users->addUser($username, $email, $password, $role);
+            $form = new Zend_Form;
+            
+            $username = new Zend_Form_Element_Text('username');
+            $username->setLabel('Username');
+            $form->addElement($username);
+
+            $password = new Zend_Form_Element_Text('password');
+            $password->setLabel('Password');
+            $form->addElement($password);
+
+            $submit = new Zend_Form_Element_Button('register');
+            $submit->setLabel('Register'); 
+            $form->addElement($submit);
+
+            $this->view->form = $form;
+        }
+
+
+
     
   
     public function deleteAction($id)
